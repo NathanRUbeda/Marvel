39910a227e33820d700a8d22ed67553ebde08de8
nathanrubeda <ubedanathan@gmail.com>
2024-10-15 12:39:05 -0400

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Nathan Ubeda on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarvelTests/MarvelTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarvelTests.swift
+//  MarvelTests
+//
+//  Created by Nathan Ubeda on 10/15/24.
+//
+
+import Testing
+
+struct MarvelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
